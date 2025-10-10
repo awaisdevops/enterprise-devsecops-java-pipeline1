30823d33a5695aa5a28b11968a5e4f750c3312c7
Awais Akram <awais@devops-portfolio.localdomain>
2025-10-10 11:51:18 +0500

    Configure health check poing in deployment

--- src/test/java/com/example/MyServiceIT.java
+++ src/test/java/com/example/MyServiceIT.java
@@ -3,9 +3,9 @@ package com.example;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -22,7 +22,7 @@ import static org.junit.Assert.*;
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MyServiceIT {
 
-    @LocalServerPort
+    @Value("${local.server.port}")
     private int port;
 
     @Autowired
